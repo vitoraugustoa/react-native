59b0b728fa3871eb71f51a4051af0f9ac8caca04
Vitor Lopes <42519619+vitoraugustoa@users.noreply.github.com>
2021-04-01 14:34:11 -0300

    Update firebaseConnection.ts

--- firebaseApp/src/database/firebaseConnection.ts
+++ firebaseApp/src/database/firebaseConnection.ts
@@ -3,13 +3,7 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIRJLCp2ZWucGQw8ttyCC-hVpZIdQEFKk",
-  authDomain: "meuapp-f1bcb.firebaseapp.com",
-  projectId: "meuapp-f1bcb",
-  storageBucket: "meuapp-f1bcb.appspot.com",
-  messagingSenderId: "742589614032",
-  appId: "1:742589614032:web:4d5cc2741316a001119bd5",
-  measurementId: "G-XKEK34T32T"
+  // Configurações do firebase
 };
 
 if(!firebase.apps.length)
